ed66e86ba3d0f39d64efe601616fe76441157cee
skrudals <skrudals0423@gmail.com>
2025-02-22 18:34:25 +0900

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace C_study20250222
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
